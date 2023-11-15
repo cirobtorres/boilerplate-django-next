8477f9bca71c7474f269577c351e720d7dba598c
cirobtorres <82688118+cirobtorres@users.noreply.github.com>
2023-11-14 21:37:22 -0300

    Update settings.py

--- backend/djangoapp/core/settings.py
+++ backend/djangoapp/core/settings.py
@@ -8,7 +8,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+from dotenv import load_dotenv
 import os
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,8 +25,6 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'INSECURE')
 SECRET_KEY_JWT = os.getenv('SECRET_KEY_JWT', 'INSECURE')
 
 DEBUG = os.getenv('DEBUG') == 'True' or False
-
-DEBUG = True
 
 ALLOWED_HOSTS = [
     h.strip()
