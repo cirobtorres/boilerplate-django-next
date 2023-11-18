5f3e92214d8151abbdf1214565bd0f8c5311dcdc
cirobtorres <82688118+cirobtorres@users.noreply.github.com>
2023-11-18 18:39:57 -0300

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    webpackDevMiddleware: config => {
+        config.watchOptions = {
+            poll: 5000, // Check for changes every 5 seconds
+            aggregateTimeout: 300, // delay before rebuilding
+        };
+        return config;
+    },
+}
 
 module.exports = nextConfig
