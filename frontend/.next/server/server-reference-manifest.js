6f8766d742bfeb732b84610a3426b39d849bca28
ciro <cbtorres89@gmail.com>
2023-12-17 00:15:54 -0300

    Minor changes

---
+++ frontend/.next/server/server-reference-manifest.js
@@ -0,0 +1,2 @@
+self.__RSC_SERVER_MANIFEST =
+  '{"node":{},"edge":{},"encryptionKey":"/x4qnw0KbqzKHbvf8VF9oefGc3gVp+ec/4vomd9bj1w="}';
--- frontend/type.d.ts
+++ frontend/type.d.ts
@@ -1,1 +1,3 @@
-type User = {};
+type User = {
+  firstName: string;
+};
